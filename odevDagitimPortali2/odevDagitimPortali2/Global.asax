b21da82af3d01df9d9575bca9b25317a2534f437
ekzaN <ihsanmertorengul@icloud.com>
2022-04-30 17:48:15 +0300

    Odev Dagitim Portali Web Api Kodlaması

---
+++ odevDagitimPortali2/odevDagitimPortali2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="odevDagitimPortali2.WebApiApplication" Language="C#" %>
---
+++ odevDagitimPortali2/odevDagitimPortali2/ViewModel/OgrenciModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace odevDagitimPortali2.ViewModel
+{
+    public class OgrenciModel
+    {
+        public string ogrId { get; set; }
+        public string ogrNo { get; set; }
+        public string ogrAdsoyad { get; set; }
+        public System.DateTime ogrDogTarih { get; set; }
+        public string ogrFoto { get; set; }
+    }
+}
